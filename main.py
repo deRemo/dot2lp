9b37f14e7a391dd0f21bb8bffdd3f855bb4e8e63
Remo <andreoliremo@gmail.com>
2022-11-02 00:07:40 +0100

    parsing node/vertex information too

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ if not nx.is_directed_acyclic_graph(G):
 
 
 with open("test1.lp", "w") as f:
+    # Vertex info constraints
+    for v in list(G.nodes(data=True)):
+        f.write(f"V_{v[0]}_c = {v[1]['c']}\n")
+        f.write(f"V_{v[0]}_M = {v[1]['M']}\n\n")
+
+    # Edge constraints
     f.write("\\ Use as conditional constraints\n")
     for e in list(nx.edge_bfs(G)):
         f.write(f"ADJ_{e[0]}_{e[1]} = 1\n")
