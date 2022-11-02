97d7d98022648fadb3e0fc6bdb9fd6ae74bf321e
Remo <andreoliremo@gmail.com>
2022-11-02 13:54:50 +0100

    get input dot file from argv

--- main.py
+++ main.py
@@ -2,13 +2,21 @@
 import networkx as nx
 import sys
 
-G = nx.drawing.nx_pydot.read_dot("test1.dot")
+if len(sys.argv) != 2:
+    print(f"USAGE: {sys.argv[0]} /path/to/file.dot")
+    sys.exit(-1)
+
+dotpath = sys.argv[1]
+assert ".dot" in dotpath, "Pass a valid .dot file"
+
+G = nx.drawing.nx_pydot.read_dot(dotpath)
 
 if not nx.is_directed_acyclic_graph(G):
     sys.exit(-1)
 
 
-with open("test1.lp", "w") as f:
+lppath = dotpath.replace(".dot", ".lp")
+with open(lppath, "w") as f:
     # Vertex info constraints
     for v in list(G.nodes(data=True)):
         f.write(f"V_{v[0]}_c = {v[1]['c']}\n")
